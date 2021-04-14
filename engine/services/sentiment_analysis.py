9fa4eac6ab697647da23026e0b075d80d4812bd1
Rafael-PC <rafael_rx@hotmail.com>
2021-04-14 14:44:25 -0300

    modularize code

--- engine/services/sentiment_analysis.py
+++ engine/services/sentiment_analysis.py
@@ -1,9 +1,12 @@
 from google.cloud import language_v1
 import os
 
-def sentiment_analysis(text):
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
-    client = language_v1.LanguageServiceClient()
-    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
-    return sentiment.score, sentiment.magnitude
+class GNaturalLanguage:
+    def __init__(self):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+        self.client = language_v1.LanguageServiceClient()
+    
+    def sentiment_analysis(self, text):        
+        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+        sentiment = self.client.analyze_sentiment(request={'document': document}).document_sentiment
+        return sentiment.score, sentiment.magnitude
--- engine/services/ytb.py
+++ engine/services/ytb.py
@@ -2,15 +2,21 @@ import requests
 import config
 import json
 
-def _get_live_data(url):
-    r = requests.get(url)
-    data = json.loads(r.text)
-    return data["items"]
+class Youtube:
+    def __init__(self):
+        self.video_id = config.VIDEO_ID
+        self.api_key = config.API_KEY
+        self.max_results = config.MAX_RESULTS
 
-def get_live_comments():
-    url = f'{config.BASE_URL}/commentThreads?part=snippet%2Creplies&videoId={config.VIDEO_ID}&key={config.API_KEY}&maxResults={config.MAX_RESULTS}'
-    return _get_live_data(url)
+    def _get_live_data(self, url):
+        r = requests.get(url)
+        data = json.loads(r.text)
+        return data["items"]
 
-def get_live_details():
-    url = f'{config.BASE_URL}/videos?part=statistics,snippet&id={config.VIDEO_ID}&key={config.API_KEY}&maxResults={config.MAX_RESULTS}'
-    return _get_live_data(url)
+    def get_live_comments(self):
+        url = f'{config.BASE_URL}/commentThreads?part=snippet%2Creplies&videoId={self.video_id}&key={self.api_key}&maxResults={self.max_results}'
+        return self._get_live_data(url)
+
+    def get_live_details(self):
+        url = f'{config.BASE_URL}/videos?part=statistics,snippet&id={self.video_id}&key={self.api_key}&maxResults={self.max_results}'
+        return self._get_live_data(url)
